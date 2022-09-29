21b623281106d18e1bd6333a58461db5ba6bad85
Kelrison Coelho <kelrison_coelho@hotmail.com>
2022-09-28 21:03:29 -0400

    Inclusão da classe Jogador

--- funcoes.js
+++ funcoes.js
@@ -1,1 +1,38 @@
-alert ("Hello")
+class jogador {
+nome;
+posicao;
+dt_nascimento;
+nacionalidade;
+altura;
+peso;
+
+constructor(nome,posicao,dt_nascimento,nacionalidade,altura,peso){
+  this.nome = nome;
+  this.posicao = posicao;
+  this.dt_nascimento = dt_nascimento;
+  this.nacionalidade = nacionalidade;
+  this.altura = altura;
+  this.peso = peso;
+}
+
+setNome(novo_nome){
+  this.nome = novo_nome;
+}
+
+getNome(){
+  return this.nome;
+}
+
+getCalculaIdade(){
+var ano = this.dt_nascimento[6]+this.dt_nascimento[7]+this.dt_nascimento[8]+this.dt_nascimento[9];
+var idade = (2022 - ano)
+console.log (idade)
+
+}
+
+}
+
+var player = new jogador('Kelrison','volante','20/12/1989','Brasileiro',1.69,70);
+
+player.setNome("coelho");
+console.log(player.getCalculaIdade());
